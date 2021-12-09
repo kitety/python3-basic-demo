e4c89067459ff11f85f0c25e0c25833c310e99b5
kitety <aizaizuori@foxmail.com>
2021-12-10 00:06:47 +0800

    first commit

---
+++ 2/1.py
@@ -0,0 +1,20 @@
+print(type(1))
+
+print(type(-1))
+
+print(type(1.1))
+
+print(type(1.12222222))
+
+print(1+0.1)
+print(type(1+0.1))
+print(type(1+1.0))
+# 2.0 浮点数
+
+print(type(1*1.0))
+print(type(1*1))
+
+# 除法是浮点类型
+print(type(1/1))
+print(1/1)  # 1.0
+print(1//1)  # 整除 1
