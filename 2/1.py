4bbfb30c40e676874bb3092becb75932a5d84119
念歌 <niange@qunhemail.com>
2021-12-10 13:16:27 +0800

    feat: advance

--- 2/1.py
+++ 2/1.py
@@ -18,3 +18,26 @@ print(type(1*1))
 print(type(1/1))
 print(1/1)  # 1.0
 print(1//1)  # 整除 1
+'''
+/ 除法 浮点
+// 整除 int
+'''
+
+
+# 进制 2 8 10 16
+# 进制转换
+print(0b10)  # 2进制
+print(0o10)  # 8进制
+print(0x10)  # 16进制
+
+# 转换
+'''
+type 查看类型
+bin 转换为二进制
+oct 转换为八进制
+int 转换为十进制
+hex 转换为十六进制
+'''
+print(bin(1230))  # 2进制
+print(hex(0o10))  # 8进制
+print(oct(0x10))  # 16进制
