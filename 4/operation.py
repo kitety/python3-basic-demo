33d6ba7a56e67dd7aa1cea135a9cab9b7844aa12
kitety <aizaizuori@foxmail.com>
2024-09-30 18:46:53 +0800

    operation

---
+++ 4/operation.py
@@ -0,0 +1,15 @@
+'''
++ - * / % ** //
+'''
+
+a = 10
+b = 20
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(2 % b)
+# 幂运算
+print(a ** b)
+# 地板除
+print(a // b)
---
+++ 4/variable.py
@@ -0,0 +1,19 @@
+a = [1, 2, 3, 4, 5, 6]
+b = [1, 2, 3]
+print(a * 3 + b)
+
+# 命名可读性
+number = 123
+
+'''
+值类型(不可变) int str tuple
+引用类型 list dict set
+'''
+# 'ab'[0]='c'  error
+
+'''
+id 内存地址
+'''
+
+a = 123
+print(id(a))
